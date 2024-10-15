80ffbb8d33a7a5e68b9ca826baeda3d39a023259
nlamri <159049715+nlamri@users.noreply.github.com>
2024-10-15 18:01:36 +0200

    Update icons name - 3.1.3-3.1.4

--- src/sql/plugins/quicklinks/plugin/init_db_quicklinks.sql
+++ src/sql/plugins/quicklinks/plugin/init_db_quicklinks.sql
@@ -1,13 +1,14 @@
 --
 -- Init table quicklinks_action
 --
-INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES (1, 'quicklinks.action.modify.name', 'quicklinks.action.modify.description', 'jsp/admin/plugins/quicklinks/ModifyQuicklinks.jsp', 'icon-edit icon-white', 'MODIFY', 0);
-INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES (2, 'quicklinks.action.modify.name', 'quicklinks.action.modify.description', 'jsp/admin/plugins/quicklinks/ModifyQuicklinks.jsp', 'icon-edit icon-white', 'MODIFY', 1);
-INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES (9, 'quicklinks.action.disable.name', 'quicklinks.action.disable.description', 'jsp/admin/plugins/quicklinks/ConfirmDisableQuicklinks.jsp', 'icon-remove icon-white', 'CHANGE_STATE', 1);
-INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES (10, 'quicklinks.action.enable.name', 'quicklinks.action.enable.description', 'jsp/admin/plugins/quicklinks/DoEnableQuicklinks.jsp', 'icon-ok icon-white', 'CHANGE_STATE', 0);
-INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES (11, 'quicklinks.action.copy.name', 'quicklinks.action.copy.description', 'jsp/admin/plugins/quicklinks/DoCopyQuicklinks.jsp', 'icon-move icon-white', 'COPY', 0);
-INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES (12, 'quicklinks.action.copy.name', 'quicklinks.action.copy.description', 'jsp/admin/plugins/quicklinks/DoCopyQuicklinks.jsp', 'icon-move icon-white', 'COPY', 1);
-INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES (13, 'quicklinks.action.delete.name', 'quicklinks.action.delete.description', 'jsp/admin/plugins/quicklinks/ConfirmRemoveQuicklinks.jsp', 'icon-trash icon-white', 'DELETE', 0);
+INSERT INTO quicklinks_action (id_action, name_key, description_key, action_url, icon_url, action_permission, quicklinks_state) VALUES
+	(1, 'quicklinks.action.modify.name', 'quicklinks.action.modify.description', 'jsp/admin/plugins/quicklinks/ModifyQuicklinks.jsp', 'pencil', 'MODIFY', 0),
+	(2, 'quicklinks.action.modify.name', 'quicklinks.action.modify.description', 'jsp/admin/plugins/quicklinks/ModifyQuicklinks.jsp', 'pencil', 'MODIFY', 1),
+	(9, 'quicklinks.action.disable.name', 'quicklinks.action.disable.description', 'jsp/admin/plugins/quicklinks/ConfirmDisableQuicklinks.jsp', 'x', 'CHANGE_STATE', 1),
+	(10, 'quicklinks.action.enable.name', 'quicklinks.action.enable.description', 'jsp/admin/plugins/quicklinks/DoEnableQuicklinks.jsp', 'check', 'CHANGE_STATE', 0),
+	(11, 'quicklinks.action.copy.name', 'quicklinks.action.copy.description', 'jsp/admin/plugins/quicklinks/DoCopyQuicklinks.jsp', 'copy', 'COPY', 0),
+	(12, 'quicklinks.action.copy.name', 'quicklinks.action.copy.description', 'jsp/admin/plugins/quicklinks/DoCopyQuicklinks.jsp', 'copy', 'COPY', 1),
+	(13, 'quicklinks.action.delete.name', 'quicklinks.action.delete.description', 'jsp/admin/plugins/quicklinks/ConfirmRemoveQuicklinks.jsp', 'trash', 'DELETE', 0);
 
 --
 -- Init table quicklinks_entry_type
